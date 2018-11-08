6451ca48bff64d1a3c106e495e879e4ff82f4dc2
Devon.Klipp <devonklipp@gmail.com>
2018-11-08 18:42:23 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   # get '/' => 'users#index'
+  get 'comments/index'
   get 'sessions/new'
   get 'stats/index'
   get 'stats/show'
@@ -9,7 +10,12 @@ Rails.application.routes.draw do
   get '/sprivia' => 'users#sprivia'
   post '/sign-in' => 'sessions#create'
 
+  get 'posts' => 'posts#index'
+  post '/posts' => 'posts#new'
+
   resources :users
+  resources :posts
+  resources :comments
   resources :sessions, only: [:new, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
